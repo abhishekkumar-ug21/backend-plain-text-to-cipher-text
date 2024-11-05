26fef64dd011ae567492e4db8cae57c9b17d2422
abhishekkumar-ug21 <abhishekkumar.ug21@nsut.ac.in>
2024-11-05 18:24:38 +0530

    add env in python and more

--- scripts/caesar_cipher.py
+++ scripts/caesar_cipher.py
@@ -1,16 +1,24 @@
 import sys
 
-def encrypt_caesar(text, shift=3):
-    result = ""
+def generate_cipher(text):
+    shift = 3
+    cipher_text = ""
+
     for char in text:
-        if char.isalpha():
-            shift_amount = shift if char.islower() else shift
-            result += chr((ord(char) - 65 + shift_amount) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + shift_amount) % 26 + 97)
+        if char.isupper():
+            new_char = chr((ord(char) - 65 + shift) % 26 + 65)
+            cipher_text += new_char
+        elif char.islower():
+            new_char = chr((ord(char) - 97 + shift) % 26 + 97)
+            cipher_text += new_char
         else:
-            result += char  # Non-alphabetical characters are unchanged
-    return result
+            cipher_text += char
+
+    return cipher_text
 
 if __name__ == "__main__":
-    plain_text = sys.argv[1]
-    cipher_text = encrypt_caesar(plain_text)  # Default shift is 3
-    print(cipher_text)
+    if len(sys.argv) > 1:
+        plain_text = sys.argv[1]
+        print(generate_cipher(plain_text))
+    else:
+        print("Please provide a string to encrypt.")
